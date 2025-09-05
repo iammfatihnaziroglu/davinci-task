8bd514cb3c802ae0b82f4cda03cf3877ef5a8f21
M.Fatih <m.fatihnaziroglu@gmail.com>
2025-09-05 22:44:49 +0300

    vite-tailwind template created lint

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// src/App.tsx
+export default function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-gray-600">Merhaba Da Vinci Board Game!</h1>
+    </div>
+  );
+}
